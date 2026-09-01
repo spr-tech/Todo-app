c2a27420ae0e4860755b848d8f39b0c5b685d7eb
spr-tech <adetoroshina434@gmail.com>
2026-09-01 10:48:16 +0100

    Add docker file and version badge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,20 @@ import './App.css'
 function App() {
   const [todos, setTodos] = useState([])
   const [title, setTitle] = useState('')
+  const [version, setVersion] = useState('')
 
+  // fetch the list from the API
   async function loadTodos() {
     const res = await fetch('/api/todos')
     setTodos(await res.json())
   }
 
-  useEffect(() => { loadTodos() }, [])
+  useEffect(() => {
+    loadTodos()
+    fetch('/api/health')
+      .then((res) => res.json())
+      .then((data) => setVersion(data.version))
+  }, [])
 
   async function addTodo(event) {
     event.preventDefault()
@@ -36,7 +43,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>My To-Do List</h1>
+      <h1>My To-Do List <span className="version">{version}</span></h1>
       <form onSubmit={addTodo}>
         <input
           value={title}
